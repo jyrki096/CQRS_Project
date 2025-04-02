370ca1349076682e7f248fe5da95ad33eca9ab78
Юрий <jyrki096@gmail.com>
2025-04-02 14:37:55 +0300

    Контекст БД

---
+++ Infrastructure/Data/DataBaseContext/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Application.Data.DataBaseContext;
+using Domain.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.DataBaseContext;
+
+public class ApplicationContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Topic> Topics => Set<Topic>();
+
+    public ApplicationContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+}
