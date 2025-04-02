4a4c82661d071e37e7a124f141fdadfcceac11fc
Юрий <jyrki096@gmail.com>
2025-04-02 14:04:06 +0300

    Добавлена первая сущность Topic

---
+++ Domain/Models/Topic.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Abstractions;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models;
+
+public class Topic : Entity<TopicId>
+{
+    public string Title { get; set; } = default!;
+    public DateTime EventStart { get; set; } = default!;
+    public string Summary { get; set; } = default!;
+    public string TopicType { get; set; } = default!;
+    public Location Location { get; set; } = default!;
+
+    public static Topic Create(
+        TopicId topicId, string title, DateTime eventStart,
+        string summary, string topicType, Location location)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(title);
+        ArgumentException.ThrowIfNullOrWhiteSpace(summary);
+        ArgumentException.ThrowIfNullOrWhiteSpace(topicType);
+
+        return new()
+        {
+            Id = topicId,
+            Title = title,
+            EventStart = eventStart,
+            TopicType = topicType,
+            Location = location,
+            Summary = summary
+        };
+    }
+
+}
