b551b021e61d0e87d2fe8c0f73e04f0649561923
Юрий <jyrki096@gmail.com>
2025-04-03 11:22:10 +0300

    Сервисы для БД, наполнение данными

---
+++ Infrastructure/Data/Extensions/DatabaseExtension.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure.Data.DataBaseContext;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Extensions;
+
+public static class DatabaseExtension
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using IServiceScope scope = app.Services.CreateScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.MigrateAsync().GetAwaiter().GetResult();
+
+        await SeedData(dbContext);
+    }
+
+    private static async Task SeedData(ApplicationDbContext dbContext)
+    {
+        await SeedTopicAsync(dbContext);
+    }
+
+    private static async Task SeedTopicAsync(ApplicationDbContext dbContext)
+    {
+        if (!await dbContext.Topics.AnyAsync())
+        {
+            await dbContext.Topics.AddRangeAsync(InitialData.Topics);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
