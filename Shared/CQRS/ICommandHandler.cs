8b6fa998133c6b50bc3114c3aae0d592b93c686c
Юрий <jyrki096@gmail.com>
2025-04-08 12:15:31 +0300

    Подоготовка инфраструктуры под паттерн CQRS

---
+++ Shared/CQRS/ICommandHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.CQRS;
+
+public interface ICommandHandler<in TCommand, TResponse>
+    : IRequestHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull
+{
+}
