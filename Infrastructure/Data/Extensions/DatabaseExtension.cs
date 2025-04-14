094cecfc070b21e2add55bba0e3bd47bb0e21242
Юрий <jyrki096@gmail.com>
2025-04-14 08:07:17 +0300

    FakeUsers

--- Infrastructure/Data/Extensions/DatabaseExtension.cs
+++ Infrastructure/Data/Extensions/DatabaseExtension.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Domain.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Data.Extensions;
 
@@ -9,16 +11,30 @@ public static class DatabaseExtension
         using IServiceScope scope = app.Services.CreateScope();
 
         var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        var manager = scope.ServiceProvider.GetRequiredService<UserManager<CustomIdentityUser>>();
 
         dbContext.Database.MigrateAsync().GetAwaiter().GetResult();
 
-        await SeedData(dbContext);
+        await SeedData(dbContext, manager);
     }
 
-    private static async Task SeedData(ApplicationDbContext dbContext)
+    private static async Task SeedData(ApplicationDbContext dbContext, UserManager<CustomIdentityUser> manager)
     {
         await SeedTopicAsync(dbContext);
+        await SeedUsersAsync(dbContext, manager);
     }
+
+    private static async Task SeedUsersAsync(ApplicationDbContext dbContext, UserManager<CustomIdentityUser> manager)
+    {
+        if(!manager.Users.Any())
+        {
+            foreach (var user in InitialData.Users)
+            {
+                await manager.CreateAsync(user, "qwerty123");
+            }
+        }
+    }
+
 
     private static async Task SeedTopicAsync(ApplicationDbContext dbContext)
     {
