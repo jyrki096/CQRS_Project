77780ab04e69fce260cef6cc6878f3b8f181ba33
Юрий <jyrki096@gmail.com>
2025-04-14 09:14:22 +0300

    AuthConroller with login endpoint

---
+++ Api/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Security;
+using Domain.Security.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(UserManager<CustomIdentityUser> manager) : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IResult> Login(LoginRequestDto loginRequestDto)
+    {
+        var user = await manager.FindByEmailAsync(loginRequestDto.Email);
+
+        if (user is null)
+        {
+            return Results.Unauthorized();
+        }
+
+        var result = await manager.CheckPasswordAsync(user, loginRequestDto.Password);
+
+        if (result)
+        {
+            var response = new IdentityUserResponseDto(user.UserName!, user.Email!, "jwt");
+
+            return Results.Ok(response);
+        }
+
+        return Results.Unauthorized();
+    }
+}
