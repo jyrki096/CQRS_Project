d4835d3932102fa944e0769305552958214f9f00
Юрий <jyrki096@gmail.com>
2025-04-25 12:20:42 +0300

    GetCommentsQuery

---
+++ Application/Comments/Queries/GetCommentsHandler.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dtos.Comment;
+using AutoMapper.QueryableExtensions;
+
+namespace Application.Comments.Queries;
+
+public class GetCommentsHandler(IApplicationDbContext dbContext, IMapper mapper)
+    : IQueryHandler<GetCommentsQuery, GetCommentsResult>
+{
+    public async Task<GetCommentsResult> Handle(GetCommentsQuery request, CancellationToken cancellationToken)
+    {
+        var topicId = TopicId.Of(request.TopicId);
+
+        var comments = await dbContext.Comments
+                                      .Where(c => c.CurrentTopic.Id == topicId)
+                                      .ProjectTo<CommentDto>(mapper.ConfigurationProvider)
+                                      .OrderByDescending(c => c.CreateAt)
+                                      .ToListAsync(cancellationToken);
+
+        return new GetCommentsResult(comments);
+    }
+}
---
+++ Application/Comments/Queries/GetCommentsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Dtos.Comment;
+
+namespace Application.Comments.Queries;
+
+public record GetCommentsQuery(Guid TopicId) : IQuery<GetCommentsResult>;
+
+public record GetCommentsResult(IEnumerable<CommentDto> Result);
+
