c5d4672b4ad5e0e101457063bcda12a4dd568a8d
Юрий <jyrki096@gmail.com>
2025-04-25 11:05:32 +0300

    CommentIdConfiguration для правильного маппинга CommentId в базе данных

---
+++ Infrastructure/Data/Configuration/CommentIdConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Data.Configuration;
+
+public class CommentIdConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.Property(comment => comment.Id)
+              .HasConversion(id => id.Value, value => CommentId.Of(value));
+    }
+}
